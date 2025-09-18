e41318aff726752664edbe9111e3f39fc289dc3c
Enkonix6465 <hr@enkonix.in>
2025-09-18 12:42:35 +0530

    Update InvoicePreview.tsx

--- client/components/invoice/InvoicePreview.tsx
+++ client/components/invoice/InvoicePreview.tsx
@@ -62,7 +62,7 @@ const InvoicePreview = forwardRef<HTMLDivElement, Props>(({ data }, ref) => {
                   alt={data.status}
                   style={{
                     width: 124,
-                    height: 93,
+                    height: 124,
                     opacity: 0.9,
                     pointerEvents: "none",
                     mixBlendMode: "normal",
